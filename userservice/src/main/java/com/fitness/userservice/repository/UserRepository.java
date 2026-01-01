8535e9b16177dbbc3a5af790f1d9ccc53cbfec8c
MOHAMMAD SAMEER <81515912+mohammadsameer1999@users.noreply.github.com>
2026-01-01 23:05:48 +0530

    Delete userservice directory

--- userservice/src/main/java/com/fitness/userservice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fitness.userservice.repository;
-
-import com.fitness.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-}
--- userservice/src/main/java/com/fitness/userservice/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.fitness.userservice.service;
-
-import com.fitness.userservice.dto.UserRequest;
-import com.fitness.userservice.dto.UserResponse;
-
-public interface UserService {
-    public UserResponse createUser (UserRequest request);
-}
