b8467990fb29acc50ca3b9febd1cc1259b26ac1e
MOHAMMAD SAMEER <81515912+mohammadsameer1999@users.noreply.github.com>
2026-01-01 23:05:26 +0530

    Delete activity-service directory

--- activity-service/src/main/java/com/activity/repository/ActivityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.activity.repository;
-
-import com.activity.model.Activity;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ActivityRepository extends MongoRepository<Activity,String> {
-}
